71ef55579df59a02d5cf5f3de25f0d3a2646de98
rashmikaz <phrashmika@gmail.com>
2025-01-17 15:37:37 +0530

    sign out

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,35 +1,29 @@
 import React from "react";
 import { Link } from "react-router";
 import "./Navbar.css";
+import { Power } from "lucide-react";
+
 export function Navbar() {
   return (
     <>
-      <header className="bg-green-500 shadow-lg">
-        <nav className="px-4 py-3">
-          <ul className="flex text-white space-x-4">
-            <h1 className="mt-1 text-xl mr-4 ">Green Shadow</h1>
+     <header className="bg-green-500 shadow-lg sticky top-0 z-50">
+        <nav className="px-4 py-3 flex items-center">
+          <div className="flex items-center text-white space-x-4">
+            <h1 className="mt-1 text-xl mr-6">Green Shadow</h1>
             <Link to="/" className="custom-link">
               Dashboard
+              </Link>
+              <Link to="/vehicle" className="custom-link">
+              Vehicle Details
             </Link>
-            <Link to="/field" className="custom-link">
-              Field
-            </Link>
-            <Link to="/crop" className="custom-link">
-              Crop
-            </Link>
-            <Link to="/staff" className="custom-link">
-              Staff
-            </Link>
-            <Link to="/equipment" className="custom-link">
-              Equipment
-            </Link>
-            <Link to="/log" className="custom-link">
-              Log
-            </Link>
-            <Link to="/vehicle" className="custom-link">
-              Vehicle
-            </Link>
-          </ul>
+          </div>
+          <button
+            className="ml-auto flex items-center gap-2  bg-white text-green-500 px-4 py-2 rounded-md font-medium hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-green-400"
+            onClick={() => alert("Signed Out")}
+          >
+            <Power className="h-5 w-5" />
+            Sign Out
+          </button>
         </nav>
       </header>
     </>
